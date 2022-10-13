401344330bdd9e0322905f6a9100d45803ebc0d3
Blue-Yeti <112828764+Blue-Yeti@users.noreply.github.com>
2022-10-13 03:43:14 +0300

    23123

--- js/modal.js
+++ js/modal.js
@@ -5,9 +5,13 @@
       modal: document.querySelector("[data-modal]"),
     };
   
+    
+
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
+
+
     function toggleModal() {
       // refs.modal.classList.toggle("is-hidden");
       refs.modal.classList.toggle("is-hidden");
